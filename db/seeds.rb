44fab5fc862b8b171df81c5ba9c821645c3b72b0
Konstantin Samarin <konstsamari@gmail.com>
2022-05-06 18:38:05 +0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Transaction.destroy_all
+
+NUM_USERS = 2
+NUM_EARNINGS = 10
+NUM_SPENDINGS = 5
+
+NUM_USERS.times { |i| User.create(name: "User #{i}") }
+
+User.all.each do |user|
+  NUM_EARNINGS.times do |i|
+    Transaction.create(user: user, date: Time.now, amount: rand(1..100), name: "Earning #{i}")
+  end
+
+  NUM_SPENDINGS.times do |i|
+    Transaction.create(user: user, date: Time.now, amount: rand(-100..-1), name: "Spending #{i}")
+  end
+end
