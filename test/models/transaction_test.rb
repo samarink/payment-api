b03845d3d8c52972e2ef870a3cb24e604c85fa68
Konstantin Samarin <konstsamari@gmail.com>
2022-05-07 15:07:17 +0700

    add unit tests

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @transaction = transactions(:one)
+  end
+
+  test 'valid with right attributes' do
+    assert @transaction.valid?
+  end
+
+  test 'invalid if any of the attributes are missing' do
+    @transaction.name = nil
+    @transaction.date = nil
+    @transaction.amount = nil
+    @transaction.user = nil
+
+    refute @transaction.valid?, 'saved invalid transaction'
+    assert_not_nil @transaction.errors[:name], 'no validation error for name present'
+    assert_not_nil @transaction.errors[:date], 'no validation error for date present'
+    assert_not_nil @transaction.errors[:amount], 'no validation error for amount present'
+    assert_not_nil @transaction.errors[:user], 'no validation error for user present'
+  end
+
+  test '#in_the_period_of' do
+    filtered = Transaction.in_the_period_of('2022-04-01'.to_date..'2022-06-01'.to_date)
+
+    assert_includes filtered, transactions(:one)
+    assert_includes filtered, transactions(:two)
+    refute_includes filtered, transactions(:three)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+  end
+
+  test 'valid with name' do
+    assert @user.valid?
+  end
+
+  test 'invalid without name' do
+    @user.name = nil
+    refute @user.valid?, 'saved user without a name'
+    assert_not_nil @user.errors[:name], 'no validation error for name present'
+  end
+
+  test '#transactions' do
+    assert_equal 3, @user.transactions.size
+  end
+
+  test '#balance' do
+    assert_equal 3, @user.balance
+  end
+
+  test '#balance_at_point_it_time' do
+    assert_equal 3, @user.balance_at_point_in_time('2022-05-06'.to_date)
+    assert_equal 0, @user.balance_at_point_in_time('1900-01-01'.to_date)
+  end
+
+  test '#transactions_in_the_period_of' do
+    result = @user.transactions_in_the_period_of('2022-04-01', '2022-06-01')
+
+    assert_equal 1, result[:balance_at_start_of_the_period]
+    assert_equal 3, result[:balance_at_end_of_the_period]
+    assert_equal 2, result[:transactions].size
+  end
 end
